04a7fed0a1483e808dfed4fc8b63bf7abe41b141
nairo-mudumane <nairomudumane@outlook.com>
2023-07-05 10:55:04 +0200

    update: tasks filter

--- src/controllers/task/get-all.ts
+++ src/controllers/task/get-all.ts
@@ -3,40 +3,51 @@ import { IAuthRequest, ISortOder, ITask, IUser } from "../../@types";
 import { taskModel, userModel } from "../../schemas";
 import { DEFAULT_ERROR_MESSAGES } from "../../constants";
 
-type IQuery = { category: string };
+type IQuery = { category: string; is_done: "0" | "1" };
 
 export async function getAll(request: IAuthRequest, response: Response) {
   let user: IUser | null = null;
   let tasks: Array<ITask> = [];
-  const acceptedFilters = ["category"];
+
+  let filters: { [key: string]: string } = {};
+  const acceptedFilters = ["category", "is_done"];
   const sort = { completeAt: -1, createdAt: -1 } as ISortOder;
+
   const reqUser = request.user;
   const query = request.query as Partial<IQuery>;
 
   try {
     user = await userModel.findOne(reqUser);
-    if (!user)
-      return response
-        .status(400)
-        .json({ message: DEFAULT_ERROR_MESSAGES.unknownOriginOrUser });
+    if (!user) throw new Error(DEFAULT_ERROR_MESSAGES.unknownOriginOrUser);
 
-    if (Object.keys(query).length === 0)
-      tasks = await taskModel.find().sort(sort);
-    else {
-      let filters: { [key: string]: string } = {};
-
+    if (Object.keys(query).length > 0) {
       for (const key of Object.keys(query)) {
         if (!acceptedFilters.includes(key))
-          return response
-            .status(400)
-            .json({ message: `filter: ${key} not supported` });
+          throw new Error(`filtro: '${key}' não suportado`);
 
         // @ts-ignore
-        filters[key] = String(decodeURI(query[key]));
-
-        tasks = await taskModel.find(filters).sort(sort);
+        if (key !== "is_done") filters[key] = String(decodeURI(query[key]));
+        else {
+          console.log("entrou no is done");
+          // @ts-ignore
+          if (query.is_done === "0") filters["isDone"] = false;
+          // @ts-ignore
+          else if (query.is_done === "1") filters["isDone"] = true;
+          else
+            throw new Error(
+              `filtro: '${key}' tem valor não suportado. Os valores esperados são: '0' ou '1'`
+            );
+        }
       }
     }
+  } catch (error) {
+    return response.status(400).json({ message: (error as Error).message });
+  }
+
+  try {
+    if (Object.keys(filters).length > 0)
+      tasks = await taskModel.find(filters).sort(sort);
+    else tasks = await taskModel.find().sort(sort);
 
     return response.status(200).json({ message: "ok", data: tasks });
   } catch (error) {
